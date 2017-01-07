1db028555d79dc1a9ed73777e13ebf0e355a4904
Sebastien <bossi.sebastien@gmail.com>
2017-01-08 00:55:06 +0100

    Almoooost

--- site/web/app/themes/marty/assets/scripts/main.js
+++ site/web/app/themes/marty/assets/scripts/main.js
@@ -20,18 +20,23 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
-      /*DEFER LOADING*/
-      function downloadJSAtOnload() {
-var element = document.createElement("script");
-element.src = "/app/themes/marty/dist/scripts/defer.js";
-document.body.appendChild(element);
-}
-if (window.addEventListener)
-window.addEventListener("load", downloadJSAtOnload, false);
-else if (window.attachEvent)
-window.attachEvent("onload", downloadJSAtOnload);
-else window.onload = downloadJSAtOnload;
-
+        /*SDK FB*/
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        /* TYPEKIT */
+        (function(d) {
+        var config = {
+          kitId: 'qwl5oio',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
 /* SLIDEOUT MENU*/
 var slideout = new Slideout({
      'panel': document.getElementById('panel'),
