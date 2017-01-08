96dce8297d8d9b87486e9eb0441383e552574c3d
Sebastien <bossi.sebastien@gmail.com>
2017-01-08 01:06:11 +0100

    Almooost

--- site/web/app/themes/marty/assets/scripts/main.js
+++ site/web/app/themes/marty/assets/scripts/main.js
@@ -21,13 +21,17 @@
       init: function() {
         // JavaScript to be fired on all pages
         /*SDK FB*/
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.0";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        $(window).load( function(){
+    (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s);
+         js.id = id;
+         js.onload = function() { fbAsyncInit(); }
+         js.src = "//connect.facebook.net/fr_FR/sdk.js"; 
+         fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+});
         /* TYPEKIT */
         (function(d) {
         var config = {
