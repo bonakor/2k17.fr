5a2b513720befaeac4226b509efd163dea3711ea
Sebastien <bossi.sebastien@gmail.com>
2017-01-11 22:32:03 +0100

    v0.9.91

--- site/web/app/themes/marty/templates/head.php
+++ site/web/app/themes/marty/templates/head.php
@@ -4,5 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="8vuaqaUivQkPGjaNxoSeT65TRemHfPz7MXXlsVXTW3c" />
   <meta name="theme-color" content="#7647f9">
+  <meta property="fb:pages" content="381390085538284" />
   <?php wp_head(); ?>
 </head>
