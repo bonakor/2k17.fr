77a81f0d03f9cf73bbddbbb3c0790e3f00776f3d
Sebastien <bossi.sebastien@gmail.com>
2017-01-12 19:02:05 +0100

    New home

--- site/web/app/themes/marty/front-page.php
+++ site/web/app/themes/marty/front-page.php
@@ -19,6 +19,6 @@
 </section> */ ?>
 <?php get_template_part('templates/front', 'una'); ?>
 <? /* php get_template_part('templates/front', 'quadra'); ?>
-<?php get_template_part('templates/front', 'diapa'); ?>
-<?php get_template_part('templates/front', 'lista'); */?>
+<?php get_template_part('templates/front', 'diapa');*/ ?>
+<?php get_template_part('templates/front', 'lista'); ?>
 <?php get_template_part('templates/front', 'categoria'); ?>
