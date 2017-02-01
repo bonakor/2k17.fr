155281eed6d4c7d9f5a22ee82c8d77715f0e739f
Sebastien <bossi.sebastien@gmail.com>
2017-02-01 21:26:00 +0100

    Update lista home

--- site/web/app/themes/marty/templates/front-lista.php
+++ site/web/app/themes/marty/templates/front-lista.php
@@ -45,5 +45,5 @@ endforeach;
 
 wp_reset_postdata();
 } */?>
-<?php echo do_shortcode('[ajax_load_more cache="true" cache_id="8601006533" preloaded="true" repeater="template_4" post_type="post"]'; ?>
+<?php echo do_shortcode('[ajax_load_more cache="true" preloaded="true" repeater="template_4" post_type="post"]'); ?>
 </section>
